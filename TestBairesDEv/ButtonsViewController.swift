4fe854bade0829113c5b33de864a3ba6e408854e
Fernando <archilo20@hotmail.com>
2019-01-19 10:33:28 -0600

    initial commit

---
+++ TestBairesDEv/ButtonsViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ButtonsViewController.swift
+//  TestBairesDEv
+//
+//  Created by Fernando Archila on 1/19/19.
+//  Copyright © 2019 Fernando Archila. All rights reserved.
+//
+
+import Foundation
---
+++ TestBairesDEv/CellNumbers.swift
@@ -0,0 +1,24 @@
+//
+//  CellNumbers.swift
+//  TestBairesDEv
+//
+//  Created by Fernando Archila on 1/19/19.
+//  Copyright © 2019 Fernando Archila. All rights reserved.
+//
+
+import UIKit
+
+class CellNumbers: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
